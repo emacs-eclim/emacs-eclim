97d8f2c56c3ddf1296cb22de9a4242d40b58c28d
Nelson Loyola <nloyola@gmail.com>
2017-01-01 11:59:33 -0700

    Fixed Commentary section in eclim.el
    This way the description is shown correctly on Melpa.

--- eclim.el
+++ eclim.el
@@ -29,7 +29,12 @@
 ;; "eclim/command-name", like eclim/project-list.
 ;;
 ;;; Commentary:
-;; Eclim
+;;
+;; Emacs-eclim uses the Eclim Server to integrate eclipse with Emacs.  This project wants to bring
+;; some of the invaluable features from Eclipse to Emacs.  Please note, the eclim package is limited
+;; to mostly Java support at this time.
+;;
+;;; Code:
 
 (eval-when-compile (require 'cl))
 (require 'cl-lib)
