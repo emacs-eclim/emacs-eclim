1ce064368b86a086ac0c3906f47f8e3c3a61fd74
Nelson Loyola <nloyola@gmail.com>
2017-03-07 12:21:24 -0700

    Another attempt to fix coveralls reporting

--- test/specs/undercover-init.el
+++ test/specs/undercover-init.el
@@ -1,4 +1,4 @@
 (when (require 'undercover nil t)
-  (undercover "*.el" (:exclude "*test*.el") (:send-report nil)))
+  (undercover "*.el" (:exclude "*test*.el") (:report-file "emacs-coveralls.json") (:send-report nil)))
 
 (provide 'undercover-init.el)
